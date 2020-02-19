0a22709d7f3139e28f8c8f46043bb44ee8a437c6
Ubuntu <ubuntu@ip-172-31-16-126.ap-southeast-1.compute.internal>
2020-02-19 11:19:57 +0000

    modify max_seq_len=10 in get_layer_train_time

--- models/utils/device_util/device_util.py
+++ models/utils/device_util/device_util.py
@@ -128,7 +128,7 @@ class Device_Util:
             in_size /= 1000
             out_size /= 10000
         batch_size /= 50
-        seq_len /= 20
+        seq_len /= 10
         model = models.load_model(os.path.join('model/lookup_table', device, '{}.h5'.format(layer)))
         output = models.load_model(os.path.join('model/lookup_table', device, 'output.h5'))
         x = [[in_size, out_size, batch_size, seq_len]]
